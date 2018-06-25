e71287fcce0d0b581d29457bdc0dbf25ade8fefa
Dominick23 <dominick232323@gmail.com>
2018-06-24 23:14:42 -0600

    6/24/18 III

--- ForgottenRealmsMap/code.swift
+++ ForgottenRealmsMap/code.swift
@@ -11,7 +11,7 @@ import Foundation
 
 /*
  
- - change plist to use exceptions instead of allowing all websites or the app will get rejected
+ - do I need to change plist to use exceptions instead of allowing all websites 
  - when I resized the images to allow for the navigation controller bar the buttons didn't line up anymore...
  - Check 6/24/18
  
